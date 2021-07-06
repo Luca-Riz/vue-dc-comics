d2186f338f8f297e48f4c2a8357b3dd2119feb84
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-06 18:07:47 +0200

    add part of footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,43 @@
 <template>
     <footer>
-        <div class="container footer">
-            <ul>
-                <li v-for="(link,index) in links" :key="index">
-                    <img :src="link.src" alt="#">
-                    <a :href="link.url"> {{ link.text }} </a>
-                </li>
-            </ul>
+        <!-- inizio navbar footer -->
+        <div class="main-top">
+            <div class="container footer-top">
+                <ul>
+                    <li v-for="(link,index) in links" :key="index">
+                        <img :src="link.src" alt="#">
+                        <a :href="link.url"> {{ link.text }} </a>
+                    </li>
+                </ul>
+            </div>
         </div>
+        <!-- fine navbar footer -->
+
+        <!-- inizio main footer -->
+        <div class="main-main-footer">
+            <div class="container main-footer">
+                <!-- inizio main nav footer -->
+                <nav></nav>
+                <!-- fine main nav footer -->
+
+                <!-- logo main footer -->
+                <div class="logo"></div>
+                <!-- fine logo main footer -->
+
+            </div>
+        </div>
+        <!-- fine main footer -->
+
+        <!-- inizio bottom footer -->
+        <div class="main-bottom">
+            <div class="container bottom-footer">
+
+
+            </div>
+        </div>
+        <!-- fine bottom footer -->
+
+
     </footer>
 </template>
 
@@ -57,36 +87,45 @@ export default {
 <style lang="scss" scoped>
 @import '../style/vars.scss';
 
-footer{
+.main-top{
     background-color: $blue-dc;
     height: $std-height;
 
-    .footer {
+    .footer-top {
         height: 100%;
         display: flex;
         justify-content: space-around;
         align-items: center;
 
+            ul {
+                list-style: none;
 
-        ul {
-            list-style: none;
-    
-            li {
-                display: inline-block;
-                margin: 15px;
+                li {
+                    display: inline-block;
+                    margin: 15px;
+                    }
+
+
+                a {                
+                    text-decoration: none; 
+                    text-transform: uppercase;     
+                    color: white;
+                    font-weight: 700;                      
                 }
-    
-    
-            a {                
-                text-decoration: none; 
-                text-transform: uppercase;     
-                color: white;
-                font-weight: 700;                      
-            }
-        }    
+            }    
     }
-
 }
+
+.main-main-footer {
+    background-image: url("../assets/img/footer-bg.jpg");
+    height: 500px; //! da togliere
+}
+
+.main-bottom {
+    height: $std-height;
+    background-color: $grey-dc;
+}
+
 
 
 </style>
