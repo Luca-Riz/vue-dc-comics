9a98486db115ae342dc9ede539946332569cc845
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-06 15:11:21 +0200

    add nav elements

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+
+    <Products/>
+
+    <Footer/> 
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
+// import Products from './components/Products.vue';
+// import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    // Products,
+    // Footer
+
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import 'style/commons.scss';
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,108 @@
+<template>
+    <header>
+        <a href="#">
+            <img src="@/assets/img/dc-logo.png" alt="logo dc">
+            <!-- oppure
+            <img src="../assets/images/logo.png" alt=""> -->
+        </a>
+        <nav>
+            <ul>
+                <!-- <li v-for="(link,index) in links" :key="index" :class="{ active: link.current }"><a :href="link.url">{{ link.text }} </a></li> -->
+                <!-- oppure -->
+                <li v-for="(link,index) in links" :key="index" :class="link.current ? 'active' : '' "><a :href="link.url">{{ link.text }} </a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            links: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Tv',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/vars.scss';
+header{
+    text-align: center;
+
+    ul {
+        list-style: none;
+
+        li {
+            display: inline-block;
+            padding: 10px;
+
+            a {
+                color: $primary;
+                text-decoration: none;
+                font-size: $font_primary;
+            }
+
+             &:hover,
+             &.active {
+                    background-color: lightgrey;
+             }
+
+        }
+
+    }
+}
+
+</style>
